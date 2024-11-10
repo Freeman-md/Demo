b92df60aace6abb4c092fe0c393ebdb3a57908ba
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2024-11-10 06:28:42 +0000

    update: using TimerTrigger function, configure a blob output binding to write to a file in azure storage (azurite for development)

--- LogTimerFunction.cs
+++ LogTimerFunction.cs
@@ -1,9 +1,16 @@
 using System;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
 namespace Demo
 {
+    public class BlobResponse {
+        [BlobOutput("output-container/output-file.txt", Connection = "AzureWebJobsStorage")]
+        public string BlobContent { get; set; }
+
+        public HttpResponseData HttpResponseData { get; set; }
+    }
     public class LogTimerFunction
     {
         private readonly ILogger _logger;
@@ -13,10 +20,17 @@ namespace Demo
             _logger = logger;
         }
 
-        [Function("LogTimerFunction")]
-        public void Run([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer)
+        [Function("LogTimeFunction")]
+        public BlobResponse Run(
+        [TimerTrigger("*/5 * * * * *")] TimerInfo myTimer,
+        FunctionContext executionContext)
         {
-            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+             var response = new BlobResponse
+            {
+                BlobContent = "This is output to a blob!",
+            };
+
+            _logger.LogInformation("Data written to output blob");
 
             if (myTimer.ScheduleStatus is not null)
             {
@@ -26,6 +40,8 @@ namespace Demo
             {
                 _logger.LogInformation("Schedule status information is unavailable.");
             }
+
+            return response;
         }
     }
 }
